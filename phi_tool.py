2b33c4f8d7e10d9e1e3331e7035946e365be071b
phimes <44431261+re0phimes@users.noreply.github.com>
2022-11-17 09:42:34 +0000

    fix: use datetime repalce pandas.datatime

--- phi_tool.py
+++ phi_tool.py
@@ -17,5 +17,19 @@ def datelist(self, beginDate, endDate):
     return date_l
 
 
-if __name__ =='__main__':
-    None 
+def caltime(func):
+    def inner(*args, **kwargs):
+        start = datetime.now()
+        res = func(*args, **kwargs)
+        end = datetime.now()
+        print("methods: %s ,运行共计耗时: %s s"%(func.__name__,  end - start))
+        return res
+    return inner
+
+# if __name__ =='__main__':
+    # @calcMethodsTimes
+    # def test():
+    #     print('test')
+    
+    # test()
+
--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
-# -*- ecoding: utf-8 -*-
-# @ModuleName: setup
-# @Function: 
-# @Author: ctx_phi
-# @Craete Time: 2021/11/4 10:51
+from setuptools import setup, find_packages
+
+print(find_packages())
+
+requires = [
+    'requests>=2.27.1',
+    'loguru>=0.6.0',
+    'lxml==4.6.3',
+    'pydantic==1.9.1',
+    'tqdm==4.62.2',
+]
 
 
-import setuptools
-
-with open("README.md", "r") as fh:
-  long_description = fh.read()
-
-setuptools.setup(
-  name="phi_tools",
-  version="0.0.2",
-  author="phimes",
-  author_email="phimes@163.com",
-  description="A collections of often use functions",
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  url="https://github.com/re0phimes/phi-s_ultis",
-  packages=setuptools.find_packages(),
-  classifiers=[
-  "Programming Language :: Python :: 3",
-  "License :: OSI Approved :: MIT License",
-  "Operating System :: OS Independent",
-  ],
+setup(
+    name="Encrawler",
+    version="0.1.14",
+    packages=find_packages(),
+    description="增加境外bing news search",
+    author="phimes",
+    author_email="phimes@163.com",
 )
